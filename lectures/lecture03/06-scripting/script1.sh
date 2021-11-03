4e707e57ca1db942f3aecfd55239a50014e3f83d
bhansen3 <bhansen3@uw.edu>
2021-11-03 12:06:26 -0700

    updated11-2

---
+++ lectures/lecture03/06-scripting/script1.sh
@@ -0,0 +1,20 @@
+	 which python
+	 conda activate base
+	 pwd
+	 cd /lectures/lecture03
+	 cd ~/lectures/lecture03
+	 cd 02-directories
+	 conda activate base
+	 conda activate base
+	 cd lectures/lecture03/03-redirection/
+	 ls ..slides/images
+	 ls ../slides/images
+	 ls ../slides/images | grep jpg
+	 cd ..
+	 cd 04-vim
+	 pwd
+	 vi anthony.txt
+	 cd ..
+	 cd 05-history/
+	 cd ../06-scripting/
+	 fc -ln -20
